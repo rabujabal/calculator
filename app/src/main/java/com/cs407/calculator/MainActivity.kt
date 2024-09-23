60fb42286c4d15a4f27cec757bd3c193cdbdc1a9
Raghad Abujabal <Raghad.abujabal@gmail.com>
2024-09-23 12:58:18 -0500

    fixed the app and made an op fun

--- app/src/main/java/com/cs407/calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/calculator/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.cs407.calculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,33 +26,51 @@ class MainActivity : AppCompatActivity() {
         }
         val firstNumber = findViewById<EditText>(R.id.myTextField)
         val secondNumber = findViewById<EditText>(R.id.myTextField2)
+        val result = findViewById<TextView>(R.id.myTextField)
         val addButton = findViewById<Button>(R.id.button)
+        fun operatorAction(operator: String) {
+            val  num1 = firstNumber.text.toString().toFloatOrNull()?: throw NumberFormatException("Invalid number format")
+            val num2 = secondNumber.text.toString().toFloatOrNull()?: throw NumberFormatException("Invalid number format")
+            if(num2 == 0.0F){
+                Toast.makeText(this, "cannot divide by 0", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            var res: Float = 0.0f
+            when(operator)
+            {
+                "+"->res = num1+num2
+                "-"->res = num1-num2
+                "*"->res = num1*num2
+                "/"->res = num1/num2
+            }
+            val intent = Intent(this, Result::class.java)
+            //pass the user to a new activity
+            intent.putExtra("result", res)
+            //start the activity
+            startActivity(intent)
+        }
         addButton.setOnClickListener{
-            val  num1 = firstNumber.text.toString()
-            val num2 = secondNumber.text.toString()
-            val result = num1 + num2
+            operatorAction("+")
         }
 
         val subButton = findViewById<Button>(R.id.button2)
         subButton.setOnClickListener{
-            val num1 = firstNumber.text.toString()
-            val num2 = secondNumber.text.toString()
-            val result = num1.minus(num2)
+            operatorAction("-")
             }
 
 
         val mulButton = findViewById<Button>(R.id.button3)
         mulButton.setOnClickListener{
-            val  num1 = firstNumber.text.toString()
-            val num2 = secondNumber.text.toString()
-            val result = num1.times(num2)
+            operatorAction("*")
         }
         val divButton = findViewById<Button>(R.id.button4)
         divButton.setOnClickListener{
-            val  num1 = firstNumber.text.toString()
-            val num2 = secondNumber.text.toString()
-            val result = num1 / num2
+            operatorAction("/")
         }
+
+
+
     }
 
 
@@ -59,30 +81,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-private operator fun String.times(num2: String): Int {
-    val num1 = this.toIntOrNull()?: throw NumberFormatException("Invalid number format")
-    val numb2 = num2.toIntOrNull()?: throw NumberFormatException("Invalid number format")
 
-        return num1*numb2
-    };
-
-
-private operator fun String.minus(num2: String): Int {
-    val num1 = this.toIntOrNull()?: throw NumberFormatException("Invalid number format")
-    val numb2 = num2.toIntOrNull()?: throw NumberFormatException("Invalid number format")
-        return num1 - numb2
-    }
-
-
-private operator fun String.div(num2: String): Int {
-
-    val num1 = this.toIntOrNull()?: throw NumberFormatException("Invalid number format")
-    val numb2 = num2.toIntOrNull()?: throw NumberFormatException("Invalid number format")
-    if(numb2 == 0) {
-        Toast.makeText(this, numb2, Toast.LENGTH_SHORT).show()
-    }
-    return num1 / numb2
-    }
 
 
 
